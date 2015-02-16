26e657b0cb7f71168ff18bccf2946d8af7977d4a
dSkurt <darr@kth.se>
2015-02-16 14:45:29 +0100

    test push daniel

--- FaceOn/src/id2216/faceon/MainActivity.java
+++ FaceOn/src/id2216/faceon/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
-
+    //Daniel testing :D
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
